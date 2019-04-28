9681adf051dc40f86ebaf923668f15f0c00f3a2b
mingshaofeng <mingsfeng@foxmail.com>
2019-04-28 17:31:26 +0800

    对于本次提交的说明

---
+++ 多线程.py
@@ -0,0 +1,33 @@
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QListWidget, QGridLayout
+import sys
+import time
+
+
+class WinForm(QWidget):
+
+    def __init__(self, parent=None):
+        super(WinForm, self).__init__(parent)
+        self.setWindowTitle("实时刷新界面例子")
+        self.listFile = QListWidget()
+        self.btnStart = QPushButton('开始')
+
+        layout = QGridLayout(self)
+        layout.addWidget(self.listFile, 0, 0, 1, 2)
+        layout.addWidget(self.btnStart, 1, 1)
+        self.setLayout(layout)
+
+        self.btnStart.clicked.connect(self.slotAdd)
+
+    def slotAdd(self):
+        for n in range(10):
+            str_n = 'File index {0}'.format(n)
+            self.listFile.addItem(str_n)
+            QApplication.processEvents()
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    form = WinForm()
+    form.show()
+    sys.exit(app.exec_())
