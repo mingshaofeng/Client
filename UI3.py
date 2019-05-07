3faad77893ce3890ee95ee489edfdd7609da5f97
mingshaofeng <mingsfeng@foxmail.com>
2019-05-07 21:39:14 +0800

    对布局进行了透明化处理

--- UI3.py
+++ UI3.py
@@ -2,7 +2,7 @@ import sys,pymysql
 
 from functools import partial
 from PyQt5.Qt import QWidget
-from PyQt5 import QtGui,QtWidgets
+from PyQt5 import QtGui,QtWidgets,QtCore
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QFrame,QApplication,QDialog, QDialogButtonBox,
         QMessageBox,QVBoxLayout, QLineEdit,QTableWidgetItem,QTableWidget,QHBoxLayout,QHeaderView)
@@ -16,6 +16,7 @@ class Table_UI(QWidget):
     def initUI(self):
         self.setWindowTitle("数据库列表")
         self.resize(1000,800)
+        self.setObjectName('win')
 
 
         self.setWindowIcon(QtGui.QIcon('images/tubiao.ico'))
@@ -55,7 +56,7 @@ class Table_UI(QWidget):
         self.search_icon = QtWidgets.QLabel(chr(0xf002) + ' ' + '搜索  ')
         self.search_icon.setFont(qtawesome.font('fa', 16))
         self.qle = QLineEdit()
-        self.qle.setPlaceholderText("输入歌手、歌曲或用户，回车进行搜索")
+        self.qle.setPlaceholderText("输入视频名称、动作数字标签，QUERY键进行搜索")
         buttonBox = QDialogButtonBox()
         #增删查改四个按钮
         self.addButton = buttonBox.addButton("&ADD",QDialogButtonBox.ActionRole)
@@ -87,15 +88,29 @@ class Table_UI(QWidget):
         self.deleteButton.setFont(font)
         self.inquireButton.setFont(font)
 
+        #网格布局
+        self.net_widget = QtWidgets.QWidget()
+        self.net_layout = QtWidgets.QGridLayout()#搜索部件的布局
+        self.net_widget.setLayout(self.net_layout)
+        self.net_layout.addWidget(self.search_icon,0,0,1,1)
+        self.net_layout.addWidget(self.qle,0,1,1,8)
+
 
         #垂直布局
         layout = QVBoxLayout()
-
-        layout.addWidget(self.search_icon)
-        layout.addWidget(self.qle)
+        layout.addWidget(self.net_widget)
+        #layout.addWidget(self.search_icon)
+        #layout.addWidget(self.qle)
         layout.addWidget(buttonBox)
         layout.addWidget(self.MyTable)
         self.setLayout(layout)
+
+        #qss进行布局优化
+        self.setStyleSheet("#win{background-color: white}")#设置背景为白色
+        self.setWindowOpacity(0.9)#设置透明度
+        #self.setAttribute(QtCore.Qt.WA_TranslucentBackground)  # 设置窗口背景透明
+        #self.setWindowFlag(QtCore.Qt.FramelessWindowHint)  # 隐藏边框
+        layout.setSpacing(0)
 
 
 
@@ -103,5 +118,6 @@ class Table_UI(QWidget):
 if __name__=='__main__':
     app = QApplication(sys.argv)
     example = Table_UI()
+
     example.show()
     sys.exit(app.exec_())
